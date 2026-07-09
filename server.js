b3336076f11a0823dc6390d76ff27137693e99cf
Vxndxngxndhi27 <vandangandhi58@gmail.com>
2026-07-09 06:40:32 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
+
+app.get("/webhook", (req, res) => {
+  const mode = req.query["hub.mode"];
+  const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
+
+  if (mode && token === VERIFY_TOKEN) {
+    return res.status(200).send(challenge);
+  }
+
+  res.sendStatus(403);
+});
+
+app.post("/webhook", (req, res) => {
+  console.log(req.body);
+  res.sendStatus(200);
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server Started");
+});
